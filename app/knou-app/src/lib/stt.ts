8a03f3b5330353bc0faa55061d68f112935521fe
minhyeok shin <sinminhyeok@gmail.com>
2026-07-01 08:33:01 +0900

    STT 추상클래스 업데이트

--- app/knou-app/src/lib/stt.ts
+++ app/knou-app/src/lib/stt.ts
@@ -1,28 +1,9 @@
-import { DeviceUtil } from "@/utils/device-util";
-import { Platform } from "react-native";
-import { SttServer } from "./stt/stt.server";
-import { SttAndroid } from "./stt/stt.android";
-import { SttIOS } from "./stt/stt.ios";
-
-export class SttFactory {
-  static create(): Stt {
-    if (!DeviceUtil.isOnDevice()) {
-      // 저사양기기 서버 fallback
-      return new SttServer();
-    } else if (Platform.OS === 'android'){
-      // 안드로이드
-      return new SttAndroid();
-    } else {
-      // iOS
-      return new SttIOS();
-    }
-  }
-}
-
-
-/**
+/****
  * Speak to Text 
+ * 생성방법 : const stt = SttFactory.create();
  */
+export type SttAudioInput = Blob | File | MediaStream;
+
 export abstract class Stt {
- 
+  abstract convertText(audio: SttAudioInput): Promise<string>;
 }
